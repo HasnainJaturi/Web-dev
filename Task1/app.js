77d3d8122cd68b16905e0a57f5d5e4a3f8760b20
hasnainjaturi <hasnainjaturi99@gmail.com>
2024-09-09 09:04:01 +0500

    added

---
+++ Task1/app.js
@@ -0,0 +1,103 @@
+//Question one
+
+// alert ("Hello Word")
+
+// Question2
+
+//alert ( "Error! Please Enter a valid password")
+
+
+//Question3
+
+//alert ("Welcome to Js Land... \n Happy Coding")
+
+//Question4
+
+// alert (" Welcome to Js land")
+
+// alert (" Happy Coding")
+
+//console.log ("Hello... I can run JS through my browser console")
+
+
+
+// Variables Assignment
+
+//Question 1
+
+// var userName;
+
+//Question 2
+
+// userName = "Hasnain"
+
+// alert (userName)
+
+// Question 3
+
+
+// var Message;
+//  Message = "HELLO WORLD"
+
+//  alert (Message)
+
+
+//Question 4
+
+//  var myname = "Hasnain"
+
+//  var age= "24 years age"
+
+//  var course="Certified Mob Appilcation Development"
+
+
+// alert(myname);
+// alert(age);
+// alert(course);
+
+//Question 5
+// var pizza= "pizza \n pizz \n piz \n pi \n p"
+// alert(pizza);
+
+
+//Question 6
+// var email= "Hasnainhunzai99@gmail.com"
+
+// alert("My Email address is" + email )
+
+//Question 7
+
+
+// var book="A Smarter way to learn javascript"
+
+// alert("My book name is " + book)
+
+
+
+
+//Question 1
+
+// var age;
+
+// age=24;
+
+// alert(age);
+
+//Question 2
+
+// var visit;
+// visit= 14;
+
+// alert("You have visit" + visit + "times");
+
+
+//Question 3
+
+// var birthYear= 1995;
+// document.write("My birthday year is" + birthYear  )
+
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,71 @@
+ //alert("Hasnain")// Js Built in function
+
+               //JS User defined function
+
+              //document.write(12121) // JS Built in method
+
+             //console.log(212121) // Js Built in method
+
+
+            // variables
+            
+            // Data types
+            // string ,number
+            
+        //    var firstname="Muhammad"
+        //    var lastname="Faraz"
+        //    var a= "Faraz"
+
+        //    alert( firstname+ lastname)
+
+        //    console.log(Muhammad,faraz)
+            
+        //    var num1= 10;
+        //    var num1= 12;
+        //    console.log(num1)
+
+        // var abc= 10;
+
+        // abc= 15
+
+    //  var fname= "Mr"
+    //  var lname= "ahmed"
+
+    //  console.log(fname + " " + lname)
+
+
+// var num1= 10;
+// var num2= 20;
+// console.log( typeof num1)  
+
+// variable declaration
+
+// var name;""
+
+// console.log(name)
+// // variable declaration and initialization
+
+
+// var name = "Faraz"
+           
+
+// var num = 100;
+// var num2 =num+10
+// console.log(num2)
+
+// var firstname= "John"
+// var lastname= "Doe"
+// var age= 18;
+
+// console.log(Hello Mr John Doe your age is 18)
+
+// console.log("hello Mr."+ firstname+" "+ lastname + "your" +age +" "+)
+
+// //alert( "Muhammad/n Faraz")
+
+// document.write( Muhammad <br> Faraz);
+
+
+// var name= 10
+
+
